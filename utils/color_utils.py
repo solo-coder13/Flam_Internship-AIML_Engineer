8dc750c8d536d03afa080d978d126b9bc4760c26
solo-coder13 <113525473+solo-coder13@users.noreply.github.com>
2025-06-14 18:19:46 +0530

    add color_utils

---
+++ utils/color_utils.py
@@ -0,0 +1,8 @@
+from skimage.exposure import match_histograms
+import numpy as np
+
+def harmonize_colors(fg, bg):
+    fg_np = np.array(fg)
+    bg_np = np.array(bg.resize(fg.size))
+    matched = match_histograms(fg_np, bg_np, multichannel=True)
+    return Image.fromarray(matched.astype('uint8'))
